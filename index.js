a0cdcc17070e7f85374d4f2272faada25023a35d
Siyabonga Gregory <huge.fuze@gmail.com>
2021-12-28 13:35:46 +0200

    Fix - Spelling

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ async function main() {
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
     host: "smtp.mailtrap.io",
-    port: 2525,
+    port: process.env.MAIL_PORT,
     secure: false, // true for 465, false for other ports
     auth: {
       user:  process.env.MAIL_USER, // generated ethereal user
